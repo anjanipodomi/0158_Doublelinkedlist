79873278d4c2e2d081c5f51a5829326029fb5147
anjanipodomi <anjanidihapsari@gmail.com>
2024-05-03 14:07:28 +0700

    membuat prosedur void searchData

--- doubleLL/doubleLL.cpp
+++ doubleLL/doubleLL.cpp
@@ -164,6 +164,27 @@ void revtraverse()
     }
 }
 
+void searchData()
+{
+    if (listEmpty() == true)
+    {
+        cout << "\List is empty" << endl;
+    }
+    Node* prev, *curr;
+    prev = curr = NULL;
+    cout << "\Enter the roll number of the student whose record you want to search: ";
+    int num;
+    cin >> num;
+    if (search(num, &prev, &curr) == false)
+        cout << "\nRecord not found" << endl;
+    else
+    {
+        cout << "\nRecord found" << endl;
+        cout << "\nRoll number: " << curr->noMhs << endl;
+        cout << "\nName: " << curr->name << endl;
+    }
+}
+
 int main()
 {
     
