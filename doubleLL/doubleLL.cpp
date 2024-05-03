b4832daaa5ac887333630863ba4d754e481b6655
anjanipodomi <anjanidihapsari@gmail.com>
2024-05-03 12:35:54 +0700

    menambahkan cout  "\033[31mDuplicate roll members not allowed\033[0m"

--- doubleLL/doubleLL.cpp
+++ doubleLL/doubleLL.cpp
@@ -22,7 +22,8 @@ void addNode() {
     
     if (START == NULL || newNode->noMhs <= START->noMhs) {
         if (START != NULL && newNode->noMhs == START->noMhs) {
-
+            cout << "\033[31mDuplicate roll members not allowed\033[0m" << endl;
+            return;
         }
     }
 }
