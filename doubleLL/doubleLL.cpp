6deb4845ead11a2209cec1095fd799a8c40494f3
anjanipodomi <anjanidihapsari@gmail.com>
2024-05-03 13:27:29 +0700

    membuat prosedur bool searcg previous = NULL dsn current = START, dan membuat looping while

--- doubleLL/doubleLL.cpp
+++ doubleLL/doubleLL.cpp
@@ -58,8 +58,19 @@ void addNode() {
             //
             START = newNode;
         }
-
     }
+}
+
+bool search(int rollNo, Node** previous, Node** current)
+{
+    *previous = NULL;
+    *current = START;
+    while (*current != NULL && (*current)->noMhs != rollNo)
+    {
+        *previous = *current;
+        *current = (*current)->next;
+    }
+    return (*current != NULL);
 }
 
 int main()
