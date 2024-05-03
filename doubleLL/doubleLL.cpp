ed675d81a816199b7ecef6428b5339e8d6f4ae0b
anjanipodomi <anjanidihapsari@gmail.com>
2024-05-03 12:55:34 +0700

    menambahkan looping if (current != NULL) {     current->prev = newNode;

--- doubleLL/doubleLL.cpp
+++ doubleLL/doubleLL.cpp
@@ -46,6 +46,10 @@ void addNode() {
 
         newNode->next = current;
         newNode->prev = previous;
+
+        if (current != NULL) {
+            current->prev = newNode;
+        }
     }
         
 }
